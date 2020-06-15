8b3082c3dfa7c3bf7708e6b41277251e17196848
Jakob <jakob_gauffin@hotmail.com>
2020-06-16 00:25:09 +0200

    innan jag lägger om alla bildurls till storage

--- src/components/pages/UploadPage/Gammal Upload till servern.jsx
+++ src/components/pages/UploadPage/Gammal Upload till servern.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 
-const getCloudImage = imageNum => {
+const getCloudImage = () => {
 	function arrayBufferToBase64(buffer) {
 		var binary = '';
 		var bytes = [].slice.call(new Uint8Array(buffer));
@@ -24,12 +24,12 @@ const getCloudImage = imageNum => {
 		redirect: 'follow'
 	};
 
-	fetch(`/api/assets/41`, requestOptions).then(response => {
+	fetch('/api/assets/42', requestOptions).then(response => {
 		response.arrayBuffer().then(buffer => {
 			var base64Flag = 'data:image/jpeg;base64,';
 			var imageStr = arrayBufferToBase64(buffer);
 
-			document.querySelector('#imgStream2').src =
+			document.querySelector('#imgStream').src =
 				base64Flag + imageStr;
 		});
 	});
@@ -58,76 +58,47 @@ const getCloudImage = imageNum => {
 };
 
 const Upload = () => {
-	// const fileUpload = e => {
-	// 	console.log('nåt händer');
-	// 	e.preventDefault();
-	// 	let file = document.querySelector('#file').files[0];
-	// 	console.log('OUTPUT ÄR: App -> file', file);
-
-	// 	let formData = new FormData();
-	// 	formData.append('photo', file);
-	// 	console.log('OUTPUT ÄR: App -> formData', formData);
-	// 	///////////////////////////////
-	// 	var myHeaders = new Headers();
-	// 	myHeaders.append('Authorization', 'abc123');
-
-	// 	let message = '';
-	// 	fetch('/files', {
-	// 		method: 'POST',
-	// 		body: formData,
-	// 		headers: myHeaders
-	// 	})
-	// 		.then(resp => resp.json())
-	// 		.then(resp => {
-	// 			message = resp.msg;
-	// 			alert(resp.msg);
-	// 		})
-	// 		.catch(err => console.error(err));
-	// 	console.log(message);
-	// };
-
-	const uploadToCloud = async e => {
-		console.log('upload to cloud funktionen körs');
+	const fileUpload = async e => {
+		console.log('nåt händer');
 		e.preventDefault();
-		let file = document.querySelector('#cloud-file').files[0];
+		let file = document.querySelector('#file').files[0];
 		console.log('OUTPUT ÄR: App -> file', file);
 
 		let formData = new FormData();
 		formData.append('photo', file);
 		console.log('OUTPUT ÄR: App -> formData', formData);
-
+		///////////////////////////////
 		var myHeaders = new Headers();
 		myHeaders.append('Authorization', 'abc123');
 
-		try {
-			let resp = await fetch('/api/files/cloud', {
-				method: 'POST',
-				body: formData,
-				headers: myHeaders
-			});
-			resp = await resp.json();
-			console.log('OUTPUT ÄR: Upload -> resp', resp);
-			let image = await getCloudImage();
-			// console.log('OUTPUT ÄR: Upload -> image', image);
-			// let image2 = await getCloudImage(7);
-			// console.log('OUTPUT ÄR: Upload -> image', image2);
-		} catch (err) {
-			console.error(err);
-		}
-
-		// .then(resp => resp.json())
-		// .then(resp => {
-		// 	console.log('resp', resp);
-
-		// let successMessage = `${resp.msg}. Url to image: ${resp.urlToImage}`;
-		// alert(successMessage);
-		// })
-		// .catch(err => console.error(err));
+		let message = '';
+		fetch('/api/files', {
+			method: 'POST',
+			body: formData,
+			headers: myHeaders
+		})
+			.then(resp => resp.json())
+			.then(resp => {
+				message = resp.msg;
+				alert(resp.msg);
+			})
+			.catch(err => console.error(err));
+		console.log(message);
 	};
+
+	// .then(resp => resp.json())
+	// .then(resp => {
+	// 	console.log('resp', resp);
+
+	// let successMessage = `${resp.msg}. Url to image: ${resp.urlToImage}`;
+	// alert(successMessage);
+	// })
+	// .catch(err => console.error(err));
+	// };
 
 	return (
 		<div>
-			{/* <h3>File uploadish</h3>
+			<h3>File uploadish</h3>
 			<form action="" encType="multipart/form-data" method="POST">
 				<input type="file" name="photo" id="file" />
 				<button
@@ -138,8 +109,8 @@ const Upload = () => {
 			</form>
 			<br />
 			<br />
-			<br /> */}
-			<h3>LADDA UPP TILL MOLNET</h3>
+			<br />
+			{/* <h3>LADDA UPP TILL MOLNET</h3>
 			<form encType="multipart/form-data" method="POST">
 				<label htmlFor="cloud-file" className="styled-btn">
 					Bläddra...
@@ -161,13 +132,7 @@ const Upload = () => {
 					alt="streambild"
 					css={css`width: 100%;`}
 				/>
-				<img
-					id="imgStream2"
-					src=""
-					alt="streambild"
-					css={css`width: 100%;`}
-				/>
-			</form>
+			</form> */}
 		</div>
 	);
 };
